57f8d3fb6a9ffc9fb04f9a9d4daceaf8849650be
unknown <tankovic.lukas@yahoo.com>
2021-04-01 22:04:00 +0200

    [Tankovic]

--- src/main/java/com/example/TodoApp/TodoAppApplication.java
+++ src/main/java/com/example/TodoApp/TodoAppApplication.java
@@ -1,16 +1,12 @@
 package com.example.TodoApp;
 
-import com.example.TodoApp.users.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @SpringBootApplication
-@RestController
+
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
