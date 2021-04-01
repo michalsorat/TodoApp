5aef35ac3cc82d2950459fe5b3085eb2f40e5e58
unknown <tankovic.lukas@yahoo.com>
2021-04-01 16:51:04 +0200

    [Tankovic] Fix dependencies

--- src/main/java/com/example/TodoApp/users/UserService.java
+++ src/main/java/com/example/TodoApp/users/UserService.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
