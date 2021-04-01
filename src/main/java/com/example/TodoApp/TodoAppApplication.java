d36d8a3854034e6bc394ba3b3ef2cba13b52b513
unknown <tankovic.lukas@yahoo.com>
2021-04-01 22:15:58 +0200

    asddad

--- src/main/java/com/example/TodoApp/TodoAppApplication.java
+++ src/main/java/com/example/TodoApp/TodoAppApplication.java
@@ -2,7 +2,6 @@ package com.example.TodoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,9 +11,5 @@ public class TodoAppApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(TodoAppApplication.class, args);
-	}
-	@GetMapping
-	public String hello(){
-		return "hello world";
 	}
 }
